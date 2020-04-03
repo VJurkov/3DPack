353dc952a22e0c6e53da5c61baff8ed71fd43ddc
VJurkov <valeriajurkovic@yahoo.com>
2020-04-03 17:45:09 +0200

    added timer, collecting binnies

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   public void CollisionDetected(Collision collision,FPController player)
+
+    public int numberOfCollectable = 0;
+    public float timeRemaining = 20f;
+    public UIManager uIManager;
+
+
+    private void Awake()
     {
-        if(collision.gameObject.CompareTag("Enemy"))
-        {
-            Destroy(player.gameObject);
-            // TODO game overscreen
-        }
+        uIManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+    }
+
+    public void CollisionDetected(Collision collision,FPController player)
+    {
 
         if (collision.gameObject.CompareTag("Collectable"))
         {
             Destroy(collision.gameObject);
-            // TODO add score counting
+            numberOfCollectable += 1;
+            uIManager.ShowScore(numberOfCollectable);
         }
     }
-    
+    private void FixedUpdate()
+    {
+        if (timeRemaining <= 0.0f)
+        {
+            timeRemaining = 0;
+            uIManager.ShowGameOver();
+            uIManager.UpdateTime(timeRemaining);
+        }
+        else
+        {
+            timeRemaining -= Time.deltaTime;
+            uIManager.UpdateTime(timeRemaining);
+        }
+    }
+
 }
